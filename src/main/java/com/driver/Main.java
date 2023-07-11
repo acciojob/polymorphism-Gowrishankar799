bab7fa00ed416789a3adc5c35485131bdeff79cb
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-07-11 15:26:00 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String args[]){
         Product p = new Product();
         System.out.println(p.product(4,5));
-        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1,2,));
         System.out.println(p.product(1,2));
 
     }
